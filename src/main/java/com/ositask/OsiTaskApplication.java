d51d12350c06b91b86b07e4d9a5c6bc677bdf485
ti_bonev <ti_bonev@abv.bg>
2022-06-01 22:15:06 +0300

    Initial commit of project with Spring

---
+++ src/main/java/com/ositask/OsiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ositask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsiTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsiTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ositask/OsiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ositask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsiTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
